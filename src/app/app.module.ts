5b4deca10e362a278211a572a60fdc58cfb979e7
bouquety <abouquety1@gmail.com>
2022-04-21 11:36:31 +0200

    Ajout composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { PaiementComponent } from './components/paiement/paiement.component';
+import { ValidationCommandeComponent } from './components/validation-commande/validation-commande.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ConnexionComponent,
+    PanierComponent,
+    PaiementComponent,
+    ValidationCommandeComponent
   ],
   imports: [
     BrowserModule,
