7e67ab2ea6dc1c4b5b28ef1364af6df8a896d33e
Caesar92 <cesar.gonnot@orange.fr>
2022-04-25 16:49:56 +0200

    register fini

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -4,5 +4,12 @@ export interface User {
     id: number,
     email: string,
     password: string,
+    prenom: string,
+    nom: string,
+    dateNaissance: Date,
+    addresse: string,
+    codePostal: string,
+    ville: string,
+    pays: string,
     role: Role[]
 }
