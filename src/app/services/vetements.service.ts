7550d6ccafe64cec145e7fc569e1ca7a9f5448ce
Caesar92 <cesar.gonnot@orange.fr>
2022-04-26 17:47:31 +0200

    debug

--- src/app/services/vetements.service.ts
+++ src/app/services/vetements.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Vetement } from '../interfaces/vetement';
 import {Avis} from "../interfaces/avis";
-import {observableToBeFn} from "rxjs/dist/types/internal/testing/TestScheduler";
 const API_URL = 'http://localhost:8080/api/'
 @Injectable({
   providedIn: 'root'
